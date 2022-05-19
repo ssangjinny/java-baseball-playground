68d10277cc23519fdd68102d58be67723631b35c
상진이 <bsj3232@gmail.com>
2022-05-19 22:39:09 +0900

    자바 플레이그라운드 단위 테스트 실습 substring, charat

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,8 +12,25 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
     void split_test() {
-        String[] splitString = "1,2".split(",");
-        assertThat(splitString).isEqualTo("");
+        String[] splitTestString = "1,2".split(",");
+        assertThat(splitTestString).contains("1", "2");
+        assertThat(splitTestString).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring_test() {
+        String substringTestString = "(1,2)";
+        assertThat(substringTestString.substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("IndexOutOfRange")
+    void charat_test() {
+        String charatTestString = "abc";
+        assertThatThrownBy(() -> assertThat(charatTestString.charAt(4)).isEqualTo('b'))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
     }
 }
